3ae7d2f00d27d119a85fe2c00f614d5b46b78fa1
Ilja Magunia <118304101+ilja78@users.noreply.github.com>
2022-11-17 12:44:53 +0100

    update

--- test/support/vcr_setup.rb
+++ test/support/vcr_setup.rb
@@ -13,4 +13,14 @@ VCR.configure do |config|
   sensitive_environment_variables.each do |key_name|
     config.filter_sensitive_data("<#{key_name}>") { ENV.fetch(key_name) }
   end
+
+  sensitive_environment_variables = %h[
+    INFLUX_HOST
+    INFLUX_TOKEN
+    INFLUX_ORG
+    INFLUX_BUCKET
+  ]
+  sensitive_environment_variables.each do |key_name|
+    config.filter_sensitive_data("<#{key_name}>") { ENV.fetch(key_name) }
+  end
 end
