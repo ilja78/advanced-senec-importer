aac35031960ac3ed384aef0218b4ef53ce2b6be1
Ilja Magunia <118304101+ilja78@users.noreply.github.com>
2022-11-17 14:08:34 +0100

    wh variable

--- test/support/vcr_setup.rb
+++ test/support/vcr_setup.rb
@@ -4,17 +4,7 @@ VCR.configure do |config|
   config.cassette_library_dir = 'test/cassettes'
   config.hook_into :webmock
 
-  sensitive_environment_variables = %w[
-    INFLUX_HOST
-    INFLUX_TOKEN
-    INFLUX_ORG
-    INFLUX_BUCKET
-  ]
-  sensitive_environment_variables.each do |key_name|
-    config.filter_sensitive_data("<#{key_name}>") { ENV.fetch(key_name) }
-  end
-
-  sensitive_environment_variables = %h[
+  sensitive_environment_variables = %w, %wh[
     INFLUX_HOST
     INFLUX_TOKEN
     INFLUX_ORG
