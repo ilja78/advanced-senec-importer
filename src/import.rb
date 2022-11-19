a08c9a7a2d48f65bba6618a8f2378bf16e55b854
Ilja Magunia <118304101+ilja78@users.noreply.github.com>
2022-11-19 09:01:40 +0100

    ohne def kwh

--- src/import.rb
+++ src/import.rb
@@ -54,11 +54,6 @@ class Import
     cell(row, *columns).sub(',', '.').to_f
   end
 
-  # KiloWatt-Stunde
-  def parse_kwh(row, *columns)
-    cell(row, *columns).sub(',', '.').to_f
-  end
-
   def cell(row, *columns)
     # Find column with values (can have different names)
     column = columns.find { |col| row[col] }
